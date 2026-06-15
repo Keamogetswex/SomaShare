2352961b89bc5d41b7322ba4e3b5f94f87c34c12
Keamogetswex <25302438@STADIO.ac.za>
2026-06-15 18:03:58 +0200

    Add files via upload
    Updated version of SomaShare uploading pt4

--- Program.cs
+++ Program.cs
@@ -1,90 +1,120 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using SomaShare.Infrastructure.Data;
-using SomaShare.Infrastructure.Services;
-using SomaShare.Web.Services;
-using SomaShareSS3.Models;
-using SomaShareSS3.Services.Interfaces;
+using SomaShareWebApp.Data;
+using SomaShareWebApp.Models;
+using SomaShareWebApp.Services;
+using SomaShareWebApp.Services.Interfaces;
+using ITransactionService = SomaShareWebApp.Services.ITransactionService;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
-// Database
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// Identity
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+namespace SomaShareWebApp
 {
-    options.Password.RequireDigit = true;
-    options.Password.RequireLowercase = true;
-    options.Password.RequireUppercase = true;
-    options.Password.RequireNonAlphanumeric = true;
-    options.Password.RequiredLength = 8;
-    options.SignIn.RequireConfirmedAccount = false;
-    options.User.RequireUniqueEmail = true;
-})
-.AddEntityFrameworkStores<ApplicationDbContext>()
-.AddDefaultTokenProviders();
-
-// Authentication
-builder.Services.AddCascadingAuthenticationState();
-builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<ApplicationUser>>();
-
-// Application Services
-builder.Services.AddScoped<ITextbookService, TextbookService>();
-builder.Services.AddScoped<IOfferService, OfferService>();
-builder.Services.AddScoped<IReviewService, ReviewService>();
-builder.Services.AddScoped<ITransactionService, TransactionService>();
-builder.Services.AddScoped<IWantedAdService, WantedAdService>();
-
-// Custom Services
-builder.Services.AddScoped<EmailValidationService>();
-builder.Services.AddScoped<LocalizationService>();
-
-internal class LocalizationService
-{
-}
-
-WebApplication app = builder.Build();
-
-// Database initialization
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    try
+    public class Program
     {
-        var context = services.GetRequiredService<ApplicationDbContext>();
-        var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
-        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
-        await DbInitializer.InitializeAsync(context, userManager, roleManager);
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Razor + Blazor
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            // Database
+            builder.Services.AddDbContext<ApplicationDbContext>(optionsAction: options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            // Identity
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequiredLength = 8;
+                options.SignIn.RequireConfirmedAccount = false;
+                options.User.RequireUniqueEmail = true;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+            // Authentication
+            builder.Services.AddCascadingAuthenticationState();
+            builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<ApplicationUser>>();
+
+            // Application Services
+            builder.Services.AddScoped<ITextbookService, TextbookService>();
+            builder.Services.AddScoped<IOfferService, OfferService>();
+            builder.Services.AddScoped<IReviewService, ReviewService>();
+            builder.Services.AddScoped<ITransactionService, TransactionService>();
+            builder.Services.AddScoped<IWantedAdService, WantedAdService>();
+
+            // Custom Services
+            builder.Services.AddHttpContextAccessor();
+            builder.Services.AddScoped<EmailValidationService>();
+            builder.Services.AddScoped<LocalizationService>();
+
+            var app = builder.Build();
+
+            // Database initialization
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                    DbInitializer.InitializeAsync(context, userManager, roleManager)
+                        .GetAwaiter().GetResult();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                    throw;
+                }
+            }
+
+            // Pipeline
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error", createScopeForErrors: true);
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            _ = app.MapRazorComponents<SomaShareWebApp.Blazor.Components.App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
     }
-    catch (Exception ex)
+
+    internal class Blazor
     {
-        var logger = services.GetRequiredService<ILogger<Program>>();
-        logger.LogError(ex, "An error occurred while seeding the database.");
+        internal class Components
+        {
+            internal class App
+            {
+            }
+        }
     }
 }
 
-// Configure the HTTP request pipeline
-if (!app.Environment.IsDevelopment())
+namespace SomaShareWebApp
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    app.UseHsts();
+    class Blazor
+    {
+        internal class Components
+        {
+            internal class App
+            {
+            }
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseAntiforgery();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapRazorComponents<SomaShareSS3.Blazor.Components.App>()
-    .AddInteractiveServerRenderMode();
-
-app.Run();
